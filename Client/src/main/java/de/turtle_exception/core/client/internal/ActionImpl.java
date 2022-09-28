f1d67042616de26ff172f6d07a184075b879831f
TurtleException <turtle.exception@eldritch.de>
2022-09-28 02:15:46 +0200

    Create Request class

--- Client/src/main/java/de/turtle_exception/core/client/internal/ActionImpl.java
+++ Client/src/main/java/de/turtle_exception/core/client/internal/ActionImpl.java
@@ -2,23 +2,28 @@ package de.turtle_exception.core.client.internal;
 
 import de.turtle_exception.core.client.api.requests.Action;
 import de.turtle_exception.core.client.api.TurtleClient;
+import de.turtle_exception.core.client.api.requests.Request;
+import de.turtle_exception.core.netcore.net.message.Message;
 import de.turtle_exception.core.netcore.net.route.Route;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public class ActionImpl<T> implements Action<T> {
     private final @NotNull TurtleClientImpl client;
     private final Route route;
+    private final BiFunction<Message, Request<T>, T> handler;
 
     private boolean priority = false;
     private long deadline = 0;
 
-    public ActionImpl(@NotNull TurtleClient client, Route route) {
+    public ActionImpl(@NotNull TurtleClient client, Route route, BiFunction<Message, Request<T>, T> handler) {
         this.client = (TurtleClientImpl) client;
         this.route = route;
+        this.handler = handler;
     }
 
     @Override
@@ -45,5 +50,12 @@ public class ActionImpl<T> implements Action<T> {
     public @NotNull ActionImpl<T> setPriority() {
         this.priority = true;
         return this;
+    }
+
+    public void handleResponse(Message response, Request<T> request) {
+        if (handler == null)
+            request.onSuccess(null);
+        else
+            request.onSuccess(handler.apply(response, request));
     }
 }
