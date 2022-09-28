d3be776d71eca2c965629706cdaed6ac7a56e4ae
TurtleException <turtle.exception@eldritch.de>
2022-09-29 00:51:27 +0200

    Cleanup code & minor fixes

--- NetCore/src/main/java/de/turtle_exception/core/netcore/TurtleCore.java
+++ NetCore/src/main/java/de/turtle_exception/core/netcore/TurtleCore.java
@@ -6,16 +6,9 @@ import org.jetbrains.annotations.Range;
 
 import java.util.concurrent.TimeUnit;
 
-/**
- * The TurtleCore is the heart of both Client and Server. This class is used internally so that NetCore is able to work
- * with both the Client and the Server without a circular dependency.
- * <p>If you came across this class and don't intend to change the NetCore Module you can safely ignore it. If it is
- * a required parameter type you can simply provide your Client / Server implementation class.
- */
 public abstract class TurtleCore {
     protected final RouteManager routeManager;
 
-    // default times for either in- or outbound messages
     private long defaultTimeoutInbound  = TimeUnit.SECONDS.toMillis( 8);
     private long defaultTimeoutOutbound = TimeUnit.SECONDS.toMillis(16);
 
--- NetCore/src/main/java/de/turtle_exception/core/netcore/util/Checks.java
+++ NetCore/src/main/java/de/turtle_exception/core/netcore/util/Checks.java
@@ -3,17 +3,7 @@ package de.turtle_exception.core.netcore.util;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-/**
- * A collection of basic checks that would otherwise repeat unnecessarily.
- */
 public class Checks {
-    /**
-     * Checks whether one or more objects is {@code null}. This method will only return {@code true} if none of the
-     * provided objects is {@code null}.
-     * @param obj An initial object
-     * @param other More (optional) objects
-     * @return {@code true} if, and only if, none of the provided objects is {@code null}.
-     */
     public static boolean nonNulls(Object obj, Object... other) {
         if (obj == null) return false;
         for (Object o : other)
@@ -21,19 +11,11 @@ public class Checks {
         return true;
     }
 
-    /**
-     * Checks whether an object is {@code null} and if so, throws a {@link NullPointerException} with the message
-     * "{@code X may not be null}" ({@code X} being the second parameter, this should be some sort of identifier for the
-     * provided object).
-     */
     public static void nonNull(Object obj, @NotNull String name) throws NullPointerException {
         if (obj == null)
             throw new NullPointerException(name + " may not be null");
     }
 
-    /**
-     * Returns the first parameter if it is not {@code null}, otherwise provides the second parameter.
-     */
     public static <T> @NotNull T nullOr(@Nullable T o1, @NotNull T o2) {
         return o1 != null ? o1 : o2;
     }
