dc47afc83354b0bc78d7cd95a7167f6ef1c454e5
TurtleException <turtle.exception@eldritch.de>
2022-10-05 01:00:38 +0200

    Update Routes

--- Core/src/main/java/de/turtle_exception/core/core/net/route/Routes.java
+++ Core/src/main/java/de/turtle_exception/core/core/net/route/Routes.java
@@ -10,20 +10,33 @@ public class Routes {
     public static final Route ERROR = new Route(Method.ERROR  , "error", true );
     public static final Route QUIT  = new Route(Method.META   , "quit" , false);
 
-    public static final Route EMPTY_RESPONSE = new Route(Method.RESPOND, "", true);
+    public static final Route RESPONSE = new Route(Method.RESPOND, "", true);
 
     public static class Group {
-        public static final Route GET    = new Route(Method.GET   , "groups/{id}", false);
-        public static final Route DEL    = new Route(Method.DELETE, "groups/{id}", false);
-        public static final Route CREATE = new Route(Method.PUT   , "groups"     , true );
-        public static final Route MODIFY = new Route(Method.PATCH , "groups/{id}", true );
+        public static final Route GET    = new Route(Method.GET   , "groups/{group.id}", false);
+        public static final Route DEL    = new Route(Method.DELETE, "groups/{group.id}", false);
+        public static final Route CREATE = new Route(Method.PUT   , "groups"           , true );
+        public static final Route MODIFY = new Route(Method.PATCH , "groups/{group.id}", true );
+
+        public static final Route UPDATE = new Route(Method.UPDATE, "groups/{group.id}", true );
+
+        public static final Route ADD_USER = new Route(Method.PUT   , "groups/{group}/members/{user}", false);
+        public static final Route DEL_USER = new Route(Method.DELETE, "groups/{group}/members/{user}", false);
     }
 
     public static class User {
-        public static final Route GET    = new Route(Method.GET   , "users/{id}", false);
-        public static final Route DEL    = new Route(Method.DELETE, "users/{id}", false);
-        public static final Route CREATE = new Route(Method.PUT   , "users"     , true );
-        public static final Route MODIFY = new Route(Method.PATCH , "users/{id}", true );
+        public static final Route GET    = new Route(Method.GET   , "users/{user.id}", false);
+        public static final Route DEL    = new Route(Method.DELETE, "users/{user.id}", false);
+        public static final Route CREATE = new Route(Method.PUT   , "users"          , true );
+        public static final Route MODIFY = new Route(Method.PATCH , "users/{user.id}", true );
+
+        public static final Route UPDATE = new Route(Method.UPDATE, "users/{user.id}", true );
+
+        public static final Route ADD_DISCORD = new Route(Method.PUT   , "users/{user.id}/discord/{discord.id}", false);
+        public static final Route DEL_DISCORD = new Route(Method.DELETE, "users/{user.id}/discord/{discord.id}", false);
+
+        public static final Route ADD_MINECRAFT = new Route(Method.PUT   , "users/{user.id}/minecraft/{minecraft.id}", false);
+        public static final Route DEL_MINECRAFT = new Route(Method.DELETE, "users/{user.id}/minecraft/{minecraft.id}", false);
     }
 
     /* --- */
