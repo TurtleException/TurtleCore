8e26b76221e1d6ede51b2ddf9a062d98781431b6
TurtleException <turtle.exception@eldritch.de>
2022-10-13 23:39:05 +0200

    Update PermissionHolder | Only use single parameter in hasPermission()

--- Client/src/main/java/de/turtle_exception/client/api/entities/PermissionHolder.java
+++ Client/src/main/java/de/turtle_exception/client/api/entities/PermissionHolder.java
@@ -3,16 +3,11 @@ package de.turtle_exception.client.api.entities;
 import de.turtle_exception.client.api.Permission;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
 import java.util.EnumSet;
 
 @SuppressWarnings("unused")
 public interface PermissionHolder extends Turtle {
-    boolean hasPermission(@NotNull Permission... permissions);
-
-    default boolean hasPermissions(@NotNull Collection<Permission> permissions) {
-        return this.hasPermission(permissions.toArray(new Permission[0]));
-    }
+    boolean hasPermission(@NotNull Permission permission);
 
     @NotNull EnumSet<Permission> getPermissionOverrides();
 
