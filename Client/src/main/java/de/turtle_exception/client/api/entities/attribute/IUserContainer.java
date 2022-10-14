88326357b738aa579d30db7c79886f7acc28fb5b
TurtleException <turtle.exception@eldritch.de>
2022-10-14 02:55:06 +0200

    Update IUserContainer

--- Client/src/main/java/de/turtle_exception/client/api/entities/attribute/IUserContainer.java
+++ Client/src/main/java/de/turtle_exception/client/api/entities/attribute/IUserContainer.java
@@ -7,7 +7,9 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+@SuppressWarnings("unused")
 public interface IUserContainer extends ITurtleContainer {
     @NotNull List<User> getUsers();
 
@@ -21,5 +23,21 @@ public interface IUserContainer extends ITurtleContainer {
     @Override
     default @Nullable Turtle getTurtleById(long id) {
         return getUserById(id);
+    }
+
+    /* - DISCORD & MINECRAFT - */
+
+    default @Nullable User getUserByDiscord(long snowflake) {
+        for (User user : this.getUsers())
+            if (user.getDiscordIds().contains(snowflake))
+                return user;
+        return null;
+    }
+
+    default @Nullable User getUserByMinecraft(@NotNull UUID uuid) {
+        for (User user : this.getUsers())
+            if (user.getMinecraftIds().contains(uuid))
+                return user;
+        return null;
     }
 }
