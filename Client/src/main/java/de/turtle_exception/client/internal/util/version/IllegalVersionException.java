fd15207fe17d7174778953f3c525fdc34a916555
TurtleException <turtle.exception@eldritch.de>
2022-11-03 19:20:31 +0100

    Copy utility classes

---
+++ Client/src/main/java/de/turtle_exception/client/internal/util/version/IllegalVersionException.java
@@ -0,0 +1,11 @@
+package de.turtle_exception.client.internal.util.version;
+
+public class IllegalVersionException extends RuntimeException {
+    IllegalVersionException(String raw, Throwable cause) {
+        super("'" + raw + "' could not be converted to Version", cause);
+    }
+
+    public IllegalVersionException(String message) {
+        super(message);
+    }
+}
