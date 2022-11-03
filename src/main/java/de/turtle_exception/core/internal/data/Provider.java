4cb04fc3b502766ca38ed3b1e8ab2329bdd9c930
TurtleException <turtle.exception@eldritch.de>
2022-11-03 11:42:35 +0100

    Update Provider

--- src/main/java/de/turtle_exception/core/internal/data/Provider.java
+++ src/main/java/de/turtle_exception/core/internal/data/Provider.java
@@ -1,5 +1,17 @@
 package de.turtle_exception.core.internal.data;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.annotation.AnnotationFormatError;
+import java.util.List;
+
 public interface Provider {
-    // TODO
+    <T> @Nullable T get(Class<T> type, Object primaryKey, Object... primaryKeys)
+            throws IllegalArgumentException, AnnotationFormatError;
+
+    <T> @NotNull List<T> getAll(Class<T> type)
+            throws IllegalArgumentException, AnnotationFormatError;
+
+
 }
