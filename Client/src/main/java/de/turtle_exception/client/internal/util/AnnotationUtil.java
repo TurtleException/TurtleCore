65719494ffdc74d367302996654949825ae7f6b9
TurtleException <turtle.exception@eldritch.de>
2022-11-22 20:58:33 +0100

    Update documentation | AnnotationUtil

--- Client/src/main/java/de/turtle_exception/client/internal/util/AnnotationUtil.java
+++ Client/src/main/java/de/turtle_exception/client/internal/util/AnnotationUtil.java
@@ -11,7 +11,16 @@ import java.util.List;
 public class AnnotationUtil {
     private AnnotationUtil() { }
 
-    // TODO: docs
+    /**
+     * Iterates through the entire Class hierarchy of {@code clazz} and returns the first occurrence of an annotation of
+     * the specified type.
+     * <p> Classes are added to the buffer in BFS-order (Breadth-first search), starting with the direct superclass (if
+     * present) and all implemented interfaces in the order specified by {@link Class#getInterfaces()}.
+     * @param clazz The Class object to retrieve the annotation from.
+     * @param type The Class object corresponding to the annotation type.
+     * @return The first occurrence of an annotation of the specified type.
+     * @param <T> The type of the annotation to query for and return.
+     */
     public static <T extends Annotation> @Nullable T getAnnotation(@NotNull Class<?> clazz, @NotNull Class<T> type) {
         LinkedList<Class<?>> buffer = new LinkedList<>();
         buffer.add(clazz);
