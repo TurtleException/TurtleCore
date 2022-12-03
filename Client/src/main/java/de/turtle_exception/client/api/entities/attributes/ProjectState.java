8df6e102f56599b9caee1133ef541df7154a437d
TurtleException <turtle.exception@eldritch.de>
2022-12-03 17:35:37 +0100

    Update ProjectState | Add emojis

--- Client/src/main/java/de/turtle_exception/client/api/entities/attributes/ProjectState.java
+++ Client/src/main/java/de/turtle_exception/client/api/entities/attributes/ProjectState.java
@@ -2,22 +2,23 @@ package de.turtle_exception.client.api.entities.attributes;
 
 import org.jetbrains.annotations.NotNull;
 
-// TODO: emojis
 public enum ProjectState {
-    CONCEPT((byte) 0, "Concept"),
-    PLANNING((byte) 1, "Planning"),
-    APPLICATION((byte) 2, "Application"),
-    RUNNING((byte) 3, "Running"),
-    STOPPED((byte) 4, "Stopped"),
-    CANCELED((byte) 5, "Canceled"),
-    UNDEFINED(Byte.MAX_VALUE, "Undefined");
+    CONCEPT(        (byte) 0, "Concept"    , "\uD83D\uDCDA"),
+    PLANNING(       (byte) 1, "Planning"   , "\uD83D\uDCD9"),
+    APPLICATION(    (byte) 2, "Application", "\uD83D\uDCD8"),
+    RUNNING(        (byte) 3, "Running"    , "\uD83D\uDCD7"),
+    STOPPED(        (byte) 4, "Stopped"    , "\uD83D\uDCD5"),
+    CANCELED(       (byte) 5, "Cancelled"  , "\uD83D\uDEAB"),
+    UNDEFINED(Byte.MAX_VALUE, "Undefined"  , "\u2753");
 
     private final byte code;
     private final @NotNull String name;
+    private final @NotNull String emoji;
 
-    ProjectState(byte code, @NotNull String name) {
+    ProjectState(byte code, @NotNull String name, @NotNull String emoji) {
         this.code = code;
         this.name = name;
+        this.emoji = emoji;
     }
 
     public byte getCode() {
@@ -26,6 +27,10 @@ public enum ProjectState {
 
     public @NotNull String getName() {
         return name;
+    }
+
+    public @NotNull String getEmoji() {
+        return emoji;
     }
 
     /* - - - */
