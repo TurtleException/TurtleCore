92a464ff3097532a0d25a75ed7e20f97e389e4e2
TurtleException <turtle.exception@eldritch.de>
2022-12-13 20:05:58 +0100

    Update documentation: Key

--- Client/src/main/java/de/turtle_exception/client/internal/data/annotations/Key.java
+++ Client/src/main/java/de/turtle_exception/client/internal/data/annotations/Key.java
@@ -1,5 +1,6 @@
 package de.turtle_exception.client.internal.data.annotations;
 
+import de.turtle_exception.client.api.entities.Turtle;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.annotation.ElementType;
@@ -7,10 +8,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// TODO: docs
+/**
+ * Marks an attribute (provided by a method) of a {@link Turtle} that has the {@link Resource} annotation.
+ * <p> This annotation is used to reflectively building the necessary structure of the backing database and to
+ * (de-)serialize resources from/to JSON objects.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Key {
+    /**
+     * The name of a key should be unique within a resource or any super- or sub-implementations of that resource. It is
+     * an identifier that is used for (de-)serialization and is used to create the structure of the backing database.
+     * @return Name of this key.
+     */
     @NotNull String name();
 
     /** If this is not {@link Relation#ONE_TO_ONE} the annotation must be combined with a {@link Relational}. */
